9928d24508600c88c63c9153ffe75fbe05d850a7
jonathancabujat <jonathancabujat@gmail.com>
2020-07-25 11:02:42 +0800

    Fifth commit: - added codes to implement LoaderManager in MainActivityFragment to load data from Tasks table in background.

--- app/src/main/java/com/example/tasktimer/MainActivityFragment.java
+++ app/src/main/java/com/example/tasktimer/MainActivityFragment.java
@@ -1,23 +1,93 @@
 package com.example.tasktimer;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+
+import java.security.InvalidParameterException;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "MainActivityFragment";
+
+    public static final int LOADER_ID = 0;
 
     public MainActivityFragment() {
+        Log.d(TAG, "MainActivityFragment: starts");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated: starts");
+        super.onActivityCreated(savedInstanceState);
+        LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: starts");
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        Log.d(TAG, "onCreateLoader: starts with id : " + id);
+        String[] projection = {TasksContract.Columns._ID,
+                TasksContract.Columns.TASKS_NAME,
+                TasksContract.Columns.TASK_DESCRIPTION,
+                TasksContract.Columns.TASK_SORT_ORDER};
+        String sortOrder = TasksContract.Columns.TASK_SORT_ORDER + "," + TasksContract.Columns.TASKS_NAME;
+
+        switch (id) {
+            case LOADER_ID:
+                return new CursorLoader(getActivity(),
+                        TasksContract.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        sortOrder);
+
+            default:
+                throw new InvalidParameterException(TAG + ".onCreateLoader called with invalid loader id " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, "onLoadFinished: starts");
+        int count = -1;
+
+        if (data != null) {
+            while (data.moveToNext()) {
+                for (int i = 0; i < data.getColumnCount(); i++) {
+                    Log.d(TAG, "onLoadFinished: " + data.getColumnName(i) + ": " +
+                            data.getString(i));
+                }
+                Log.d(TAG, "onLoadFinished: ======================================");
+            }
+            count = data.getCount();
+        }
+        Log.d(TAG, "onLoadFinished: count is " + count);
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        Log.d(TAG, "onLoaderReset: starts");
+
     }
 }
